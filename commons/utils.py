67b8c5f0da1c983c220c1487f2266fb432b53a33
Victor Zhao <x.zhao22@imperial.ac.uk>
2023-03-12 16:29:48 +0000

    Quarantine buggy DGL seeding method

--- commons/utils.py
+++ commons/utils.py
@@ -44,7 +44,7 @@ def seed_all(seed):
     torch.cuda.manual_seed_all(seed)
     torch.cuda.manual_seed(seed)
     np.random.seed(seed)
-    dgl.random.seed(seed)
+    # dgl.random.seed(seed)
     random.seed(seed)
     # torch.backends.cudnn.deterministic = True
     # torch.backends.cudnn.benchmark = False
